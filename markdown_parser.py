1a50c1cbb5d23b7d122f29f172bbedb3701fcc7f
Kevin Wang <kwangyuxin@hotmail.com>
2017-11-07 17:18:48 +0800

    Debug: Add prefix when upload image
    Without prefix, previously uploaed images will be overwrited by
    other image files with the same name when they are uploaded.

--- markdown_parser.py
+++ markdown_parser.py
@@ -4,6 +4,8 @@ import datetime
 import heapq
 import os
 import re
+import string
+import util
 
 import vonder
 
@@ -14,14 +16,15 @@ using_sp = config_parser.get('default', 'use')
 
 class Parser(object):
     def __init__(self, file_path):
-        self.file_path = file_path
+        self.file_path = file_path.rstrip('/')
         self.file_dir = os.path.dirname(file_path)
         self.images = []
         self.read_size = 50000
         self.pattern1 =\
             re.compile(r"!\[.*\]\(([a-zA-Z0-9\-\_\.\/]+)(\s+(\"[^\"]*\")?)?\)")
         self.pattern2 = re.compile(r"<\s*img.*\s+src\s*=\s*([\'\"])(.+?)\1.*>")
-        self.sp = getattr(vonder, using_sp).ServiceProvider()
+        prefix = self.get_upload_prefix()
+        self.sp = getattr(vonder, using_sp).ServiceProvider(prefix)
 
     def get_all_image_path(self):
         print "===>Getting all images"
@@ -70,6 +73,18 @@ class Parser(object):
             size -= self.read_size
         return content
 
+    def get_upload_prefix(self):
+        print "file_path:", self.file_path
+        if '/' in self.file_path:
+            file_name = self.file_path.rsplit('/')[-1]
+        else:
+            file_name = self.file_path
+        file_name = util.url_safe_str(file_name)
+        print "file_name:", file_name
+        now = datetime.datetime.now().strftime('%Y%m%d')
+        rand_str = util.get_random_str(string.digits + string.letters)
+        return '%s-%s-%s' % (now, rand_str, file_name)
+
     def upload_image_and_replace(self):
         print "===>Parsing markdown file"
         self.get_all_image_path()
@@ -91,6 +106,8 @@ class Parser(object):
                     print "   ===>Uploading", abs_path
                     ret_data = self.sp.upload(abs_path)
                     image_url = ret_data['download_url']
+                    if type(image_url) is unicode:
+                        image_url = image_url.encode('utf-8')
                     print "   ===>Got", image_url
                     seek_end = start
                     self.write_to_new_file(
